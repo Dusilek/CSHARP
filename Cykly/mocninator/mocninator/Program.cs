30db6f5109cf8683847bc8fe58223b11e1582470
Dusilek <88558580+Dusilek@users.noreply.github.com>
2021-11-06 20:40:03 +0100

    Add files via upload

---
+++ Cykly/mocninator/mocninator/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineApp
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+
+			Console.WriteLine("Mocninátor");
+			Console.WriteLine("==========");
+			Console.WriteLine("Zadejte základ mocniny: ");
+			int a = int.Parse(Console.ReadLine());
+			Console.WriteLine("Zadejte exponent: ");
+			int n = int.Parse(Console.ReadLine());
+
+			int vysledek = a;
+			for (int i = 0; i < (n - 1); i++)
+				vysledek = vysledek * a;
+
+			Console.WriteLine("Výsledek: {0}", vysledek);
+			Console.WriteLine("Děkuji za použití mocninátoru");
+			Console.ReadKey();
+		}
+	}
+}
