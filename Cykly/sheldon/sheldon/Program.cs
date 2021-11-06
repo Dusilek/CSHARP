51df5fd3dab14de4451ecbbdc52ea30c15344b42
Dusilek <88558580+Dusilek@users.noreply.github.com>
2021-11-06 20:41:36 +0100

    Add files via upload

---
+++ Cykly/sheldon/sheldon/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineApp
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+
+			for (int i = 0; i < 3; i++)
+			{
+				Console.WriteLine("Knock");
+			}
+			Console.WriteLine("Penny!");
+			Console.ReadKey();
+		}
+	}
+}
---
+++ Cykly/zjednodusenisoperatory/zjednodusenisoperatory/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineApp
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+
+			int bodu = 0;
+			Console.WriteLine("Je Slunce koule žhavých plynů?");
+			string spravnaOdpoved = "ano";
+			string odpoved = Console.ReadLine();
+			if (odpoved.ToLower() == spravnaOdpoved.ToLower())
+				Console.WriteLine("Správně, předtím jsi měl {0} bodů a teď máš o 1 víc!", bodu++);
+		}
+	}
+}
